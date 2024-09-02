970dcb06d4ec168d722401696f0f8401ca8498e1
alvinkenyagah <alvin47k@gmail.com>
2024-09-02 12:31:21 +0300

    Authorize post delete routes

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -33,10 +33,13 @@ const router = express.Router();
 
 router.get('/', authenticate, getTransactions);
 router.get('/:id', authenticate, getTransaction);
+router.post('/', authenticate, createTransaction);
+router.put('/:id', authenticate, updateTransaction);
+router.delete('/:id', authenticate, deleteTransaction);
 
 // Protect admin routes
-router.post('/', authenticate, authorize(['admin']), createTransaction);
-router.put('/:id', authenticate, authorize(['admin']), updateTransaction);
-router.delete('/:id', authenticate, authorize(['admin']), deleteTransaction);
+// router.post('/', authenticate, authorize(['user']), createTransaction);
+// router.put('/:id', authenticate, authorize(['user']), updateTransaction);
+// router.delete('/:id', authenticate, authorize(['user']), deleteTransaction);
 
 module.exports = router;
